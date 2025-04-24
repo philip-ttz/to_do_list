6b0b81b319d02df04821782195bb2e16b17ed3ee
philip-ttz <philip.ttz159@gmail.com>
2025-04-24 11:43:11 +0200

    js detects chosen project to display

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles/index.css';
+
+const radios = document.querySelectorAll('input[name="radio"]');
+radios.forEach(radio => {
+  radio.addEventListener('click', () => {
+    if (radio.checked) {
+      const selectedValue = radio.value;
+      console.log(`Selected radio button value: ${selectedValue}`);
+      }
+  });
+});
